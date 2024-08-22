526e238251f6545c46cd03f9e284a1d7d57692fb
aktan <kerimbekov1233@gmail.com>
2024-08-22 14:49:21 +0600

    add injactable

--- lib/core/utils/injections.dart
+++ lib/core/utils/injections.dart
@@ -1,7 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:rick_and_morty/core/utils/injections.config.dart';
-import 'di_module.dart';
 
 final getIt = GetIt.instance;
 
