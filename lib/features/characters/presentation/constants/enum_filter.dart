d2983b9ad34bb4efe73fcf8aa94b42cd75695e26
aktan <kerimbekov1233@gmail.com>
2024-08-30 15:45:17 +0600

    review enum

--- lib/features/characters/presentation/constants/enum_filter.dart
+++ lib/features/characters/presentation/constants/enum_filter.dart
@@ -2,29 +2,18 @@ enum Status { alive, dead, unknown }
 
 enum Gender { male, female, unknown }
 
-
 extension CharacterStatusExtension on Status {
-  String get description {
-    switch (this) {
-      case Status.alive:
-        return "Alive";
-      case Status.dead:
-        return "Dead";
-      case Status.unknown:
-        return "";
-    }
-  }
+  String get description => switch (this) {
+        Status.alive => "Alive",
+        Status.dead => "Dead",
+        Status.unknown => ""
+      };
 }
 
 extension CharacterGenderExtension on Gender {
-  String get description {
-    switch (this) {
-      case Gender.male:
-        return "Male";
-      case Gender.female:
-        return "Female";
-      case Gender.unknown:
-        return "";
-    }
-  }
+  String get description => switch (this) {
+        Gender.male => "Male",
+        Gender.female => "Female",
+        Gender.unknown => "",
+      };
 }
